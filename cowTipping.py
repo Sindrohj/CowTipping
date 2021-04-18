510d8f2dcb9b5e0a4e10fa7a46db7890a52711ca
Sindre Opskar Hjelle <sindrohj@stud.ntnu.no>
2021-04-18 13:53:07 +0200

    mechanics for tippiing cows

--- cowTipping.py
+++ cowTipping.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import os
 import random
 
@@ -18,13 +19,12 @@ pygame.display.set_caption("Cow Tipping")
 class Background(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
-		self.bgimage = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'backgrounds.png')), (WIDTH, HEIGHT))
+		self.bgimage = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'map.png')), (WIDTH, HEIGHT))
 		self.bgY = 0
 		self.bgX = 0
 
 	def render(self):
 		WIN.blit(self.bgimage, (self.bgX, self.bgY))
-
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
@@ -37,6 +37,8 @@ class Player(pygame.sprite.Sprite):
 		self.walking = False
 		self.direction = 'RIGHT'
 		self.pushing = False
+
+		self.push_frame = 0
 
 	def move(self):
 
@@ -57,33 +59,43 @@ class Player(pygame.sprite.Sprite):
 			
 		
 		self.rect.midbottom = self.pos
-
-	def update(self):
-		pass
 		
 
 	def push(self):
-		pass
+		if self.push_frame > 10:
+			self.push_frame = 0
+			self.pushing = False
 
-	def upgrade(self):
-		pass
+		self.push_frame += 1
+		
+
+
 
 
 class Cow(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
 		self.image =  pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'bull_idle.png')), (80, 120))
+		self.image_tipped =  pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'bull_tipped.png')), (80, 120))
 		self.rect = self.image.get_rect()
+		self.rect = self.image_tipped.get_rect()
 		self.pos = vec(0, 0)
 		self.vel = 1
-		self.sight = 2
+		self.tipped = False
 		self.direction = random.randint(0,1) # 0 for Right, 1 for Left
 		self.pos.x = random.randint(50, WIDTH - 50)
 		self.pos.y = random.randint(50, HEIGHT - 50)
 		self.moveTo = vec(random.randint(150, WIDTH - 150), random.randint(150, HEIGHT - 150))
 		
 
+	def update(self, Playergroup, player):
+		hits = pygame.sprite.spritecollide(self, Playergroup, False)
 
+		if hits and player.pushing == True:
+			print("Pushing cow")
+			self.tipped = True
+		if hits:
+			print('hit')
 
 	def move(self):
 		if self.pos.x >= (WIDTH-40):
@@ -113,9 +125,12 @@ class Cow(pygame.sprite.Sprite):
 
 		self.rect.center = self.pos	
 
-	def render(self):
-		WIN.blit(self.image, (self.pos.x, self.pos.y))
 
+	def render(self):
+		if self.tipped:
+			WIN.blit(self.image_tipped, (self.pos.x, self.pos.y))
+		else:
+			WIN.blit(self.image, (self.pos.x, self.pos.y))
 
 class EventHandler():
 	def __init__(self):
@@ -139,7 +154,7 @@ class EventHandler():
 		self.stage += 1
 		self.cow_count = 0
 		print("Stage: " + str(self.stage))
-		pygame.time.set_timer(self.cow_generation, 1500 - (50 * self.stage))
+		pygame.time.set_timer(self.cow_generation, 1)
 
 
 def main():
@@ -150,6 +165,7 @@ def main():
 	player = Player()
 	Playergroup = pygame.sprite.Group()
 	Playergroup.add(player)
+
 
 	background = Background()
 	cowsNum = 3
@@ -167,25 +183,32 @@ def main():
 				if event.key == pygame.K_n:
 					if handler.battle == True and len(Cows) == 0:
 						handler.next_stage() 
+
 				if event.key == pygame.K_SPACE:
 					handler.next_stage()
+
+				if event.key == pygame.K_k:
+					if player.pushing == False:
+						player.push()
+						player.pushing = True
 
 			if event.type == handler.cow_generation:
 				if handler.cow_count < handler.stage_cows[handler.stage - 1]:
 					cow = Cow()
 					Cows.add(cow)
 					handler.cow_count += 1 
-					print('hello')
+					
 					
 
-
+		if player.pushing == True:
+			player.push()
 		player.move()
 		background.render()
 		WIN.blit(player.image, player.rect)
 		for entity in Cows:
+			entity.update(Playergroup, player)
 			entity.move()
 			entity.render()
-			print('hello')
 		pygame.display.update()
 
 	main()
